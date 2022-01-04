de27da3ccff5bcb07dbb0c44f4e583afb549469a
Rose <ryu@wish.com>
2022-01-04 14:44:43 +0800

    init uitls

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
-/**
- * 使用 import { default as x } from 'x' 而是不是 import x from 'x'，
- * 是为了让当前版本 WebStorm 的可以正常的代码提示
- * TODO: 等待 WebStorm 支持 import x from 'x' 的代码提示
- */
-
 import classNames from "classnames";
 import datejs from "dayjs";
 import $fetch from "./bom/fetch";
 import debounce from "./func/debounce";
 import throttle from "./func/throttle";
+import sessionStorage from "./bom/sessionStorage";
+import localStorage from "./bom/localStorage";
+import promisify from "./func/promisify";
+import cookie from "./bom/cookie";
 
 export default {
   classNames,
@@ -16,4 +14,8 @@ export default {
   $fetch,
   debounce,
   throttle,
+  sessionStorage,
+  promisify,
+  localStorage,
+  cookie,
 };
